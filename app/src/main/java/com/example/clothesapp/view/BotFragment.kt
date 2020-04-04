084a7ee7ada11ee5aabf3bb8ab0868eda59ea50c
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-04-04 18:22:51 +0300

    setting up the bottom navigation of the app

---
+++ app/src/main/java/com/example/clothesapp/view/BotFragment.kt
@@ -0,0 +1,29 @@
+package com.example.clothesapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.clothesapp.R
+
+class BotFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bot, container, false)
+    }
+
+    companion object {
+
+    }
+}
